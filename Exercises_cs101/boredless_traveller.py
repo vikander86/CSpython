65a3432668355591e345c86250d7924dfa0980d0
Dofus Dorian <bager86@gmail.com>
2023-02-17 12:14:48 +0100

    Added Exercises from CS101 to a folder

---
+++ Exercises_cs101/boredless_traveller.py
@@ -0,0 +1,66 @@
+destinations = ["Paris, France","Shanghai, China","Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']] 
+
+
+def get_destination_index(destination):
+  # my_list.index(item_we_looking_for, start of list, end of list)
+  destination_index = destinations.index(destination,0,len(destinations))
+  return destination_index
+
+print(get_destination_index("Los Angeles, USA"))
+print(get_destination_index("Paris, France"))
+
+
+def get_traveler_location(traveler):
+  traveler_destination = traveler[1]
+  traveler_destination_index = get_destination_index(traveler_destination)
+  return traveler_destination_index
+
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
+
+attractions = [[] for destination in destinations]
+print(attractions)
+
+def add_attraction(destination, attraction):
+  destination_index = get_destination_index(destination)
+  attractions_for_destination = attractions[destination_index].append(attraction)
+  return
+
+add_attraction("Los Angeles, USA",['Venice Beach', ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+#print(attractions)
+
+def find_attractions(destination,interests):
+  destination_index = get_destination_index(destination)
+  attractions_in_city = attractions[destination_index]
+  attractions_with_interest = []
+  for attraction in attractions_in_city:
+    possible_attraction = attraction
+    attraction_tags = attraction[1]
+
+    for interest in interests:
+      if interest in attraction_tags:
+        attractions_with_interest.append(interest)
+  
+  return attractions_with_interest
+
+la_arts = find_attractions("Los Angeles, USA",['art'])
+print(la_arts)
+
+
+
+
+
+  
---
+++ Exercises_cs101/exercise_fix_code_1.py
@@ -0,0 +1,21 @@
+balance = 1000
+def print_balance(): 
+    print("Your balance is " + str(balance))
+
+
+def deduct(amount):
+    print("Your new balance is " + str(balance - amount))
+
+
+savings = 500
+def calculate_interest_on_savings():
+    print("You will gain interest on: " + str(savings))
+    def calculate_taxes():
+        tax_amount = savings * 0.13
+        print("You will be taxed: " + str(tax_amount))
+    calculate_taxes()
+
+print_balance()
+print("ok")
+deduct(500)
+calculate_interest_on_savings()
